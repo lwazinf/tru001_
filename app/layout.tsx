8d5972e1905d2af5f5bada4451958bbcd49ec69c
LwaziNF <lwazinf@gmail.com>
2025-01-31 18:08:32 +0200

    Working on Mobile.. no apk file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav_ from "./components/Nav_";
-import TOS_ from "./components/helpers/TOS";
+// import TOS_ from "./components/helpers/TOS";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
