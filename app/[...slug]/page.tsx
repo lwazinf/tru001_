3fd015dd21679175304c60217c7f52326ca4db86
LwaziNF <lwazinf@gmail.com>
2025-02-17 20:10:23 +0200

    Policy Function

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from "next/navigation";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 export default function CatchAllPage({ params }) {
   const path = params?.slug?.join("/") || "";
 
