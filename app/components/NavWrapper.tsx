410a0037f1bd82837496bd1f8e742577fa244e72
LwaziNF <lwazinf@gmail.com>
2025-07-10 10:18:21 +0200

    latest

--- app/components/NavWrapper.tsx
+++ app/components/NavWrapper.tsx
@@ -2,16 +2,16 @@
 
 import { usePathname } from 'next/navigation';
 import Nav_ from './Nav_';
-import { useAuth } from '@/lib/firebase/AuthContext';
+// import { useAuth } from '@/lib/firebase/AuthContext';
 
 export default function NavWrapper() {
   const pathname = usePathname();
-  const { currentUser } = useAuth();
+  // const { currentUser } = useAuth();
   
   // Don't show navigation on auth page
   if (pathname === '/auth') {
     return null;
   }
   
-  return <Nav_ user={currentUser} />;
+  return <Nav_ />;
 }
